9e249cbafcf0cdf9e186749757ff04d4a435f589
Ali Taheri <Alitaheri473@gmail.com>
2025-10-02 11:43:39 +0300

    Simple Auth pipeline done with JWT

--- backend/NotiBlock.Backend/Data/AppDbContext.cs
+++ backend/NotiBlock.Backend/Data/AppDbContext.cs
@@ -3,16 +3,12 @@ using NotiBlock.Backend.Models;
 
 namespace NotiBlock.Backend.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Recall> Recalls => Set<Recall>();
         public DbSet<AppUser> AppUsers => Set<AppUser>();
 
         public DbSet<Consumer> Consumers => Set<Consumer>();
         public DbSet<ConsumerResponse> ConsumerResponses => Set<ConsumerResponse>();
     }
-}
+} 

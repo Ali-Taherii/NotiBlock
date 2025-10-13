b8a58eb18ede3283604677afc682a1014af15c07
Ali Taheri <111309866+Ali-Taherii@users.noreply.github.com>
2025-10-13 13:30:05 +0300

    Reapply "Authentication pipeline done and fully functional with a single source of truth and role-based access"
    This reverts commit 728aa26dd5f8210682eca2cc5429ebc089d25803.

--- backend/NotiBlock.Backend/DTOs/AuthDTO.cs
+++ backend/NotiBlock.Backend/DTOs/AuthDTO.cs
@@ -3,14 +3,19 @@
     public class AuthDTO
     {
         public class AuthRegisterDto
-        { 
+        {
             public string Email { get; set; } = string.Empty;
             public string Password { get; set; } = string.Empty;
-
             public string Role { get; set; } = "Consumer"; // Default role is Consumer
         }
 
         public class AuthLoginDto
+        {
+            public string Email { get; set; } = string.Empty;
+            public string Password { get; set; } = string.Empty;
+        }
+
+        public class AuthGetRoleDto
         {
             public string Email { get; set; } = string.Empty;
             public string Password { get; set; } = string.Empty;
--- backend/NotiBlock.Backend/Models/AppUser.cs
+++ backend/NotiBlock.Backend/Models/AppUser.cs
@@ -1,10 +1,15 @@
-﻿namespace NotiBlock.Backend.Models
+﻿using System.Text.Json.Serialization;
+
+namespace NotiBlock.Backend.Models
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
+        
+        [JsonIgnore]
         public string PasswordHash { get; set; } = string.Empty;
+        
         public required string Role { get; set; }
     }
 }
